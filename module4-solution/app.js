d30e2772219af6fe69f0c20e98744e2fe85d4fea
Sandeep Deo Tripathi <41840284+Sandeeptripathi7@users.noreply.github.com>
2022-04-11 18:29:33 +0530

    Add files via upload

---
+++ module4-solution/app.js
@@ -0,0 +1,28 @@
+( function(){
+    'use strict';
+    angular.module("MenuApp", [])
+    .controller("NarrowItDownController", NarrowItDownController);
+    
+
+    return $http(...).then(function (result) {
+        // process result and only keep items that match
+        var foundItems...
+    
+        // return processed items
+        return foundItems;
+    });.$inject = ['$scope'];
+    
+
+    function NarrowItDownController($scope){
+        return $http().then(function (result) {
+            // process result and only keep items that match
+           
+        }
+        
+    }
+            
+        
+    
+
+
+})();
